0c7aba63a7f4e820a0a9b83c2555d3084f13ac9b
Vivek Jayachandran <vivek4integration@gmail.com>
2022-03-26 04:15:37 +0530

    Add script to add domain controller as SQL admin

---
+++ Scripts/DSC/SQL/AddAdmin.ps1
@@ -0,0 +1,30 @@
+﻿Configuration AddDomainAdminGroupToSQL
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        $SqlAdministratorCredential
+
+      
+    )
+
+    Import-DscResource -ModuleName 'SqlServerDsc'
+
+    node localhost
+    {
+        
+
+        SqlLogin 'Add_WindowsGroup'
+        {
+            Ensure               = 'Present'
+            Name                 = 'BizTalkTest\Domain Admins'
+            LoginType            = 'WindowsGroup'
+            ServerName           = 'TestDB'
+            InstanceName         = 'DSC'
+            PsDscRunAsCredential = $SqlAdministratorCredential
+        }
+
+     
+    }
+}
